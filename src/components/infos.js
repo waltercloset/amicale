b23abfdc181061d79e7637c5da4c4aa94f778913
foxy <foxy@MacBook-Pro-de-foxy.local>
2020-01-27 22:22:36 +0100

    fuseau horaire

--- src/components/infos.js
+++ src/components/infos.js
@@ -22,8 +22,8 @@ const Infos=(props)=>{
   const rootPath = `${__PATH_PREFIX__}/`
   let date=[];
   if(typeof(props.date)=== 'string') { date=props.date.split(' '); }
-  if(typeof(props.date)!== 'string' && props.location.pathname === rootPath) {date=props.date.toLocaleDateString('fr-FR', {weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour:'numeric', minute:'numeric'}).split(' ')}
-  if(typeof(props.date)!== 'string' && props.location.pathname !== rootPath) {date=props.date.toLocaleDateString('fr-FR', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour:'numeric', minute:'numeric'}).split(' ')}
+  if(typeof(props.date)!== 'string' && props.location.pathname === rootPath) {date=props.date.toLocaleDateString('fr-FR', {weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour:'numeric', minute:'numeric', timeZone: "Europe/Paris"}).split(' ')}
+  if(typeof(props.date)!== 'string' && props.location.pathname !== rootPath) {date=props.date.toLocaleDateString('fr-FR', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour:'numeric', minute:'numeric', timeZone: "Europe/Paris"}).split(' ')}
 
   return (
     <Inf>
